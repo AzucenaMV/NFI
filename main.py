30c1d7b404847fb59fc22293f704f368e36df303
Myrtyr <72076794+myrtyr@users.noreply.github.com>
2020-10-27 18:43:46 +0100

    Add sized trace data file
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def read_func(filename):
     textfile = open(filename,"r")
-    line1 = textfile.readline()
-    line2 = textfile.readline()
+    names = textfile.readline()
+    names = textfile.readline()
+    names = textfile.readline()
+
     for i in range(20):
         line = textfile.readline()
         splitted = line.split("\t")
+        splitted[:] = [item for item in splitted if item != '']
         print(len(splitted))
-    return [line1,line2]
+    return None
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    line1,line2 = read_func('SizedTraceData.txt')
-    print(line1.split(" "))
-    print(line2.split(" "))
+    read_func('SizedTraceData.txt')
