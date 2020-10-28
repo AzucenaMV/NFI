5e20f69cf8034a3ba57615c60871e0eb75ede85c
Myrtyr <myrtevbelkom@gmail.com>
2020-10-28 08:48:27 +0100

    Added raw trace data and plot function
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-def read_func(filename):
-    textfile = open(filename,"r")
-    names = textfile.readline()
-    names = textfile.readline()
-    names = textfile.readline()
+import numpy as np
+import matplotlib.pyplot as plt
 
-    for i in range(20):
+def read_func(filename):
+    """ Function to read data files and sort them into colors. \
+    Returns a list of sample names, colors, and the data itself as matrix."""
+    textfile = open(filename,"r")
+    textfile.readline() #first two lines are not important
+    textfile.readline()
+    names = textfile.readline() #names are separated by multiple tabs
+    names = names.split("\t")
+    names[:] = [item for item in names if item != ''] #remove empty entries (between two \t's)
+    colors = textfile.readline()
+    data = []           #fill with data
+    for i in range(121):
         line = textfile.readline()
         splitted = line.split("\t")
-        splitted[:] = [item for item in splitted if item != '']
-        print(len(splitted))
-    return None
+        #splitted.remove("")
+        splitted[:] = [int(item) for item in splitted if (item != '' and item != '\n')]
+        data.append(splitted)
+    data = np.array(data)
+    data = data.transpose()
+    return names,colors,data
+
+def plot_func(names,colors,data):
+    counter = 0
+    for i in range(len(names)-1):
+        fig = plt.figure()
+        for j in range(6):
+            plt.plot(data[:,6*i+j])
+        #plt.legend((colors[6*i:6*i+6]))
+        plt.title(names[counter])
+        plt.show()
+        counter += 1
+    pass
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    read_func('SizedTraceData.txt')
+    names, colors, data= read_func('SizedTraceData.txt')
+    plot_func(names,colors,data)
