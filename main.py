19302fc9d7dc821a05ce1bd7aa36a7588c43ef9f
Myrtyr <myrtevbelkom@gmail.com>
2020-10-29 18:52:11 +0100

    Updated some comments
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ import reading_functions as rf
 if __name__ == '__main__':
     sized_titles, sized_colors, sized_data = rf.read_func('SizedTraceData.txt')
     raw_titles, raw_colors, raw_data = rf.read_func('RawTraceData.txt')
-    rf.plot_compare(raw_data,sized_data,sized_titles,sized_colors)
-
--- reading_functions.py
+++ reading_functions.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 
 def read_func(filename):
-    """ Function to read data files and sort them into colors. \
-    Returns a list of sample names, colors, and the data itself as matrix."""
+    """ Function to read data files\
+    Returns a list of sample names, colors, \
+    and the data itself as matrix."""
     textfile = open(filename, "r")
     texts = textfile.read()
     texts = texts.split("\n")
@@ -12,45 +13,44 @@ def read_func(filename):
     titles = texts[2].split('\t')                       # get titles of files
     titles = [item for item in titles if item != '']    # remove empty entries after splitting
     colors = texts[3].split('\t')                       # get names of colors
-    data = np.zeros((len(texts[4:]),len(colors)))
+    data = np.zeros((len(texts[4:]), len(colors)))
     counter = 0
     for elt in texts[4:]:
         new = np.array(elt.split('\t'))
         new[new == ''] = 0
-        data[counter,:] = new
+        data[counter, :] = new
         counter += 1
     return titles, colors, data
 
 
 def read_func_old(filename):
-    """ Outdated: Function to read data files and sort them into colors. \
-    Returns a list of sample names, colors, and the data itself as matrix."""
-    textfile = open(filename,"r")
-    textfile.readline() #first two lines are not important
+    """ Outdated: Function to read data files using readline"""
+    textfile = open(filename, "r")
+    textfile.readline()             # first two lines are not important
     textfile.readline()
-    names = textfile.readline() #names are separated by multiple tabs
+    names = textfile.readline()     # names are separated by multiple tabs
     names = names.split("\t")
-    names[:] = [item for item in names if item != ''] #remove empty entries (between two \t's)
+    names[:] = [item for item in names if item != '']   # remove empty entries (between two \t's)
     colors = textfile.readline()
     colors = colors.split("\t")
     colors[:] = [item for item in colors if item != '']
-    data = []           #fill with data
+    data = []           # fill with data
     for i in range(5000):
         line = textfile.readline()
         splitted = line.split("\t")
         splitted[:] = [int(item) for item in splitted if (item != '' and item != '\n')]
         data.append(splitted)
     data = np.array(data)
-    return names,colors,data
+    return names, colors, data
 
 
-
-def plot_func(titles, colors, data):
+def plot_func(data, titles, colors):
+    """"Plots all single colors in lists"""
     counter = 0
     for i in range(len(titles)-1):
         for j in range(6):
             plt.figure()
-            plt.plot(data[:,6*i+j], label=str(colors[6*i+j]))
+            plt.plot(data[:, 6*i+j], label=str(colors[6*i+j]))
         plt.legend()
         plt.title(titles[counter])
         plt.show()
@@ -58,25 +58,26 @@ def plot_func(titles, colors, data):
     return None
 
 
-def plot_6C(title,colors,data):
+def plot_6C(data, title, colors):
+    """"Plots one 3x2 set of all 6 colors of one hid file"""
     fig = plt.figure()
     for i in range(6):
-        plt.subplot(3,2,i+1)
-        plt.plot(data[:,i])
-        plt.title(colors[i])
+        plt.subplot(3, 2, i+1)
+        plt.plot(data[:, i])
+        plt.title(str(colors[i]))
     plt.show()
     return fig
 
 
-def plot_compare(data_raw,data_sized,titles,colors):
-    #data_raw_short = data_raw[-len(data_sized)]
+def plot_compare(data_raw, data_sized, titles, colors):
+    # data_raw_short = data_raw[-len(data_sized)]
     difference = len(data_raw)-len(data_sized)
-    data_raw_short = data_raw[difference::,:]
+    data_raw_short = data_raw[difference::, :]
     counter = 0
     for i in range(len(titles)-1):
         for j in range(6):
             plt.figure()
-            plt.plot(data_raw_short[:,6*i+j])
+            plt.plot(data_raw_short[:, 6*i+j])
             plt.plot(data_sized[:, 6 * i + j])
             title = str(titles[i])+"_color_"+str(colors[6*i+j])
             plt.title(title)
