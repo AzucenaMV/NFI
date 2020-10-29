a19ce00adc9c46b07ce8bdb4fd5660f2ef68a741
Myrtyr <myrtevbelkom@gmail.com>
2020-10-29 10:19:22 +0100

    Improved plot functions
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -22,6 +22,29 @@ def read_func(filename):
     return titles, colors, data
 
 
+def read_func_old(filename):
+    """ Outdated: Function to read data files and sort them into colors. \
+    Returns a list of sample names, colors, and the data itself as matrix."""
+    textfile = open(filename,"r")
+    textfile.readline() #first two lines are not important
+    textfile.readline()
+    names = textfile.readline() #names are separated by multiple tabs
+    names = names.split("\t")
+    names[:] = [item for item in names if item != ''] #remove empty entries (between two \t's)
+    colors = textfile.readline()
+    colors = colors.split("\t")
+    colors[:] = [item for item in colors if item != '']
+    data = []           #fill with data
+    for i in range(5000):
+        line = textfile.readline()
+        splitted = line.split("\t")
+        splitted[:] = [int(item) for item in splitted if (item != '' and item != '\n')]
+        data.append(splitted)
+    data = np.array(data)
+    return names,colors,data
+
+
+
 def plot_func(titles, colors, data):
     counter = 0
     for i in range(len(titles)-1):
@@ -45,11 +68,27 @@ def plot_6C(title,colors,data):
     return fig
 
 
+def plot_compare(data_raw,data_sized,titles,colors):
+    #data_raw_short = data_raw[-len(data_sized)]
+    difference = len(data_raw)-len(data_sized)
+    data_raw_short = data_raw[difference::,:]
+    counter = 0
+    for i in range(len(titles)-1):
+        for j in range(6):
+            plt.figure()
+            plt.plot(data_raw_short[:,6*i+j])
+            plt.plot(data_sized[:, 6 * i + j])
+            title = str(titles[i])+"_color_"+str(colors[6*i+j])
+            plt.title(title)
+            #plt.show()
+            plt.savefig(str("raw_vs_sized"+title+".png"))
+        counter += 1
+    return None
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
     sized_titles, sized_colors, sized_data = read_func('SizedTraceData.txt')
-    plot_6C(sized_titles[0], sized_colors[0:6], sized_data[:,0:6])
     raw_titles, raw_colors, raw_data = read_func('RawTraceData.txt')
-    plot_6C(raw_titles[0], raw_colors[0:6], raw_data[:,0:6])
+    plot_compare(raw_data,sized_data,sized_titles,sized_colors)
 
