e8ef566b827caf275914d24c3c9429b4c611e856
Myrtyr <myrtevbelkom@gmail.com>
2020-10-30 10:38:34 +0100

    Added csv_read and PPF6C as csv file
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import reading_functions as rf
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     rf.csv_read("1A2_New.csv")
+    rf.csv_read("PPF6C_SPOOR.csv")
