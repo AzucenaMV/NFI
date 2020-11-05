5e1c673c21ea7f7b88992cd46ee07ff4cd9652a6
Myrtyr <myrtevbelkom@gmail.com>
2020-11-05 11:12:44 +0100

    Fixed some continuency issues (not all plotting functions taking similar input)
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ import plotting_functions as pf
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    rawdata = rf.txt_read_data('RawTraceData.txt')
     sizedtitles, sizedcolors, sizeddata = rf.txt_read_data('SizedTraceData.txt')
-    colors  = sizedcolors[0:6]
-    data_A2_1 = sizeddata[:,36:42]
-    data_A2_2 = sizeddata[:,42:48]
-    data_A2_3 = sizeddata[:,48:54]
+    # allele_dict contains bins for each allele (e.g. AMEL: {X: 81.5, Y: 87.68})
+    # dye_dict contains the color of each dye name (e.g. FL: 'b')
+    allele_dict, dye_dict = rf.xml_read_bins("PPF6C_SPOOR.xml")
     allelelist, heightlist = rf.csv_read_analyst("1A2_New.csv")
-    alleledict, dyedict = rf.xml_read_bins("PPF6C_SPOOR.xml")
-    peaks_A2_1, peaks_A2_2, peaks_A2_3 = allelelist
-    heights_A2_1, heights_A2_2, heights_A2_3 = heightlist
-    pf.plot_actual(peaks_A2_1, heights_A2_1, alleledict, dyedict, data_A2_1, colors)
-    #pf.plot_actual(peaks_A2_2, heights_A2_2, alleledict, dyedict, data_A2_2, colors)
-    #pf.plot_actual(peaks_A2_3, heights_A2_3, alleledict, dyedict, data_A2_3, colors)
+    #pf.plot_data(sizeddata,sizedtitles)
+    #pf.plot_6C(sizeddata, sizedtitles)
+    #pf.plot_raw_vs_sized(sizeddata,sizeddata,sizedtitles)
+    pf.plot_compare(allelelist[1], heightlist[1], allele_dict, dye_dict, sizeddata[:,42:48])
