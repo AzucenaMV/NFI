44ab0f3fd1ca2837470959060a7a6209cf45ac76
Myrtyr <myrtevbelkom@gmail.com>
2020-11-05 11:25:16 +0100

    Cleaned up files into folders, added more data files
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -7,8 +7,4 @@ if __name__ == '__main__':
     # allele_dict contains bins for each allele (e.g. AMEL: {X: 81.5, Y: 87.68})
     # dye_dict contains the color of each dye name (e.g. FL: 'b')
     allele_dict, dye_dict = rf.xml_read_bins("PPF6C_SPOOR.xml")
-    allelelist, heightlist = rf.csv_read_analyst("1A2_New.csv")
-    #pf.plot_data(sizeddata,sizedtitles)
-    #pf.plot_6C(sizeddata, sizedtitles)
-    #pf.plot_raw_vs_sized(sizeddata,sizeddata,sizedtitles)
-    pf.plot_compare(allelelist[1], heightlist[1], allele_dict, dye_dict, sizeddata[:,42:48])
+
