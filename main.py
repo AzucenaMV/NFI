820c2d3cf2fe2b05165fe3827070991bdf486e03
Myrtyr <myrtevbelkom@gmail.com>
2020-11-05 09:44:55 +0100

    completed plot_actual function
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import reading_functions as rf
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    rawdata = rf.read_data('RawTraceData.txt')
+    sizedtitles, sizedcolors, sizeddata = rf.read_data('SizedTraceData.txt')
+    colors  = sizedcolors[0:6]
+    data_A2_1 = sizeddata[:,36:42]
+    data_A2_2 = sizeddata[:,42:48]
+    data_A2_3 = sizeddata[:,48:54]
     allelelist, heightlist = rf.csv_read_analyst("1A2_New.csv")
     alleledict, dyedict = rf.xml_read_bins("PPF6C_SPOOR.xml")
-    print(dyedict)
-    rf.plot_actual(allelelist[0], heightlist[0], alleledict, dyedict)
+    peaks_A2_1, peaks_A2_2, peaks_A2_3 = allelelist
+    heights_A2_1, heights_A2_2, heights_A2_3 = heightlist
+    rf.plot_actual(peaks_A2_1, heights_A2_1, alleledict, dyedict, data_A2_1, colors)
+    #rf.plot_actual(peaks_A2_2, heights_A2_2, alleledict, dyedict, data_A2_2, colors)
+    #rf.plot_actual(peaks_A2_3, heights_A2_3, alleledict, dyedict, data_A2_3, colors)
--- reading_functions.py
+++ reading_functions.py
@@ -1,10 +1,7 @@
 import numpy as np
-import math as m
 import matplotlib.pyplot as plt
 import pandas as pd
-from xml.dom import minidom
-import xml.etree.ElementTree as ET
-from csv import reader
+import xml.etree.ElementTree as et
 
 
 def read_data(filename):
@@ -63,7 +60,7 @@ def plot_data(data, titles, colors):
     return None
 
 
-def plot_6C(data, title, colors):
+def plot_6C(data, colors):
     """"Plots one 3x2 set of all 6 colors of one hid file"""
     fig = plt.figure()
     for i in range(6):
@@ -81,24 +78,26 @@ def plot_compare(data_raw, data_sized, titles, colors):
     for i in range(len(titles)-1):
         for j in range(6):
             plt.figure()
-            plt.plot(data_raw_short[:, 6*i+j])
+            plt.plot(data_raw_short[:, 6 * i + j])
             plt.plot(data_sized[:, 6 * i + j])
-            title = str(titles[i])+"_color_"+str(colors[6*i+j])
+            title = str(titles[i]) + "_color_" + str(colors[6*i+j])
             plt.title(title)
             plt.show()
         counter += 1
     return None
 
+
 def csv_read_actual(filename):
     """"Read csv file of actual alleles\
      combines with ???? for peak heights?"""
     # there may be more than one sample in one file
-    pass
+    return filename
 
 
 def csv_read_analyst(filename):
     """"Read csv file of identified alleles\
-    returns list of alleles and corresponding peaks"""
+    returns list of alleles and corresponding peaks
+    allele list is nested list per sample"""
     # I can use output of xml_read_bins to find colors of alleles
     # there may be more than one sample in one file
     results = pd.read_csv(filename)
@@ -108,16 +107,16 @@ def csv_read_analyst(filename):
     allele_list = []                    # initialize small lists
     height_list = []
     for index, row in results.iterrows():
-    # iterate over all rows, because each row contains
-    # the peaks for one locus
+        # iterate over all rows, because each row contains
+        # the peaks for one locus
         if name != row[0]:                      # then start new sample
             allele_lists.append(allele_list)    # store current sample data
             height_lists.append(height_list)
             allele_list = []                    # empty lists
             height_list = []
         name = row[0]                           # then set name to current sample name
-        for i in range(2,12):
-        # go over the 10 possible locations of peak identification
+        for i in range(2, 12):
+            # go over the 10 possible locations of peak identification
             if str(row[i]) == row[i]:
                 # append value only if non-empty
                 # empty entries are converted to (float-type) NaN's by pandas
@@ -126,41 +125,50 @@ def csv_read_analyst(filename):
                 # heights are 10 indices further than
                 # their corresponding allele names
                 height_list.append(row[i+10])
+    allele_lists.append(allele_list)
+    height_lists.append(height_list)
     return allele_lists, height_lists
 
 
 def xml_read_bins(filename):
     """Read xml file for bins of each allele, \
     returns dictionary of horizontal values"""
-    thetreefile = ET.parse(filename)
+    thetreefile = et.parse(filename)
     root = thetreefile.getroot()
     # create a dictionary per color
     allele_dict = {}
     dye_dict = {}
-    for locus in root[5]: # root[5] is the loci
+    for locus in root[5]:  # root[5] is the loci
         # get the name of the marker
         current_marker = locus.find('MarkerTitle').text
         # make dict of which locus belongs to which colour
+        temp_dict = {1: 'FL-6C', 2: 'JOE-6C', 3: 'TMR-6C', 4: 'CXR-6C', 6: 'TOM-6C'}
         dye = locus.find('DyeIndex').text
-
+        dye_dict[current_marker] = temp_dict[int(dye)]
+        allele_dict[current_marker] = {}
         for allele in locus.findall('Allele'):
             # get the name of the allele (number or X/Y)
             allele_label = allele.get('Label')
             # combine marker+allele into key
-            keyname = str(current_marker)+"_"+str(allele_label)
+            # keyname = str(current_marker)+"_"+str(allele_label)
             # store the horizontal location as value
-            valuename = str(allele.get('Size'))
+            valuename = float(allele.get('Size'))
             # still not sure about difference between Size and DefSize
-            allele_dict[keyname] = valuename
-            dye_dict[keyname] = int(dye)
+            allele_dict[current_marker][allele_label] = valuename
     return allele_dict, dye_dict
 
-def plot_actual(alleles, heights, allele_dict, dye_dict):
-    plt.figure()
-    for i in range(len(alleles)):
-        # use dye_dict to plot correct color
-        plt.subplot(6,1,dye_dict[alleles[i]])
-        print(dye_dict[alleles[i]])
-        plt.plot([allele_dict[str(alleles[i])]],[heights[i]],"*")
-    plt.show()
+
+def plot_actual(alleles, heights, allele_dict, dye_dict, comparison, colors):
+    """uses both the analysts identifies peak and sized data for comparison to plot both in one image"""
+    temp_dict = {'FL-6C': 'b', 'JOE-6C': 'g', 'TMR-6C': 'y', 'CXR-6C': 'r', 'WEN-6C': 'k', 'TOM-6C': 'm'}
+    for j in range(6):
+        plt.figure()
+        plt.title(str('color is ' + str(colors[j])))
+        plt.plot(np.linspace(0, 620, len(comparison[:, j])), comparison[:, j])
+        for i in range(len(alleles)):
+            # use dye_dict to plot correct color
+            a, b = alleles[i].split("_")
+            color = temp_dict[dye_dict[a]]
+            plt.plot([allele_dict[a][b]], [heights[i]], str(color + "*"))  # add colour
+        plt.show()
     pass
