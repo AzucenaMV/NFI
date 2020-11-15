934199430a2e42ea07a2d21fbda384a4305d06e2
Myrtyr <myrtevbelkom@gmail.com>
2020-11-15 15:17:01 +0100

    Finished rewriting plot functions to data classes.
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import plotting_functions as pf
 
 
 if __name__ == '__main__':
-    sample_list = rf.txt_read_data('TraceData1.txt')
+    sample_list = rf.txt_read_data('trace_data/TraceData1.txt')
     locus_dict = rf.xml_read_bins("PPF6C_SPOOR.xml")
     print(locus_dict)
     personlist = rf.csv_read_persons(1, locus_dict)
