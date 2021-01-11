529db2ea86e8f6abd18dd421e4b76eda00055161
Myrtyr <myrtevbelkom@gmail.com>
2021-01-11 16:47:39 +0100

    Trying to get first neural nets working
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- src/training_functions.py
+++ src/training_functions.py
@@ -5,12 +5,27 @@ from src.classes import *
 
 def simplest_nn(train_input: TrainInput):
     input_image = train_input.data[0]
-    input_labels = train_input.labels[0]
-    input_image = input_image.reshape(1,161,6)
+    input_labels = train_input.labels[0].reshape(1,6)
+    print(input_image.shape)
+    input_image = input_image.reshape(1,161,6,1)
+    print(input_image.shape)
+    # should probably add more filters, but not sure how to choose it
     filter_s = 1
-    kernel_s = 5
-    conv_layer = tf.keras.layers.Conv1D(filter_s, kernel_s, activation=tf.nn.relu)
-    pool_layer = tf.keras.layers.MaxPool1D(5, strides = 5)
-    model = tf.keras.Sequential([conv_layer, conv_layer, pool_layer, conv_layer, conv_layer, pool_layer])
+    # maybe should manually set zero padding for one dimension, so 6 doesn't decrease
+    kernelsize = (3,1)
+    poolsize = (3,1)
+    # can also flatten within layers
+    # example adds dense layer, then dropout, dense, dense
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filter_s, kernelsize, activation=tf.nn.relu))
+    model.add(tf.keras.layers.MaxPool2D(poolsize))
+    model.add(tf.keras.layers.Conv2D(filter_s, kernelsize, activation=tf.nn.relu))
+    model.add(tf.keras.layers.MaxPool2D(poolsize))
+    model.add(tf.keras.layers.Conv2D(filter_s, kernelsize, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Conv2D(filter_s, kernelsize, activation=tf.nn.relu))
+    model.add(tf.keras.layers.MaxPool2D(poolsize))
+    model.add(tf.keras.layers.MaxPool2D(poolsize))
     output = model.predict(input_image)
+    print(model.summary())
+    print(output.shape)
     return output
