98c1f6f221fb784b9584856361b04aa2f64a5c5e
Myrtyr <myrtevbelkom@gmail.com>
2021-10-21 14:05:35 +0200

    Clocked them times
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -19,48 +19,46 @@ PROVEDIt_sized_trace_data_SS = ['PROVEDIt_RD14-0003(100115ADG_15sec)_sized_impro
 PROVEDIt_raw_trace_data_mix = ['PROVEDIt_RD14-0003(021016ADG_15sec)_raw_improved1.txt', 'PROVEDIt_RD14-0003(021016ADG_15sec)_raw_improved2.txt']
 PROVEDIt_raw_trace_data_SS = ['PROVEDIt_RD14-0003(100115ADG_15sec)_raw_improved1.txt', 'PROVEDIt_RD14-0003(100115ADG_15sec)_raw_improved2.txt']
 
-#
+
 if __name__ == '__main__':
     number_of_dyes = 6
     leftoffset = 500
     rightcutoff = 4800 + 500
 
-    start_time_setup = datetime.now()
     train_samples = []
     for elt in tracedata_for_training:
         train_samples += rf.txt_read_sample(elt)
     unnormalised_train, train_input, names_train = dpf.input_from_multiple_samples(train_samples, number_of_dyes, leftoffset, rightcutoff, True)
-    end_time_setup = datetime.now()
 
     test_samples = []
     for elt in tracedata_for_testing:
         test_samples += rf.txt_read_sample(elt)
     unnormalised_test, test_input, names_test = dpf.input_from_multiple_samples(test_samples, number_of_dyes, leftoffset, rightcutoff, True)
+    Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=False,epochs=100)
 
+    single_example = test_input.data[0].reshape(1,4800,6)
     start_time_training = datetime.now()
-    Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=True,epochs=400)
+    Unet.predict(single_example)
     end_time_training = datetime.now()
 
-    print('setup: ', end_time_setup-start_time_setup, 'training: ', end_time_training-start_time_training)
+    print('predicting single epg U-net: ', end_time_training-start_time_training)
 
 
+    width = 80
+    input_dim = 6*(width*2+1)
+    DTDP_train_input = dpf.DTDP_input_from_multiple_samples(train_samples, width = width)
 
-    # width = 80
-    # input_dim = 6*(width*2+1)
-    # DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
-    # score_binacc = []
-    # score_auc = []
-    # for name_index in range(len(names)):
-    #     newtestinput = classes.DTDPTrainInput([], DTDP_test_input.data[4800*name_index:4800*name_index+4800], DTDP_test_input.labels[4800*name_index:4800*name_index+4800])
-    #     FFN_model, metrics = trf.FFN([], newtestinput, weightpath="data/weights_DTDP/weights_FFN_our_data_w80_100epochs.h5", inputsize=(input_dim,), train = False, epochs=100, batchsize=100)
-    #     loss, binacc, auc = metrics
-    #     score_binacc.append(binacc)
-    #     score_auc.append(auc)
-    # datadict = {'name': names, 'bin_acc': score_binacc, 'auc': score_auc}
-    # dataframe = pd.DataFrame(datadict)
-    # dataframe.to_csv('data/results_paper/FFN_blue_dye.csv')
+    DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
+    FFN_model, metrics = trf.FFN(DTDP_train_input, DTDP_test_input, weightpath="data/weights_DTDP/weights_for_timing.h5", inputsize=(input_dim,), train = False, epochs=100, batchsize=100)
 
-    # images, prediction = ppf.combine_results_FFN(DTDP_test_input, test_input, FFN_model, input_dim)
+    just_the_data = DTDP_test_input.data
+    start_time_training = datetime.now()
+    for ind in range(4800):
+        FFN_model.predict(just_the_data[ind].reshape(1,966))
+    end_time_training = datetime.now()
+    print('predicting single epg FFN: ', end_time_training-start_time_training)
+
+    #     images, prediction = ppf.combine_results_FFN(DTDP_test_input, test_input, FFN_model, input_dim)
     # for index_of_image in range(DTDP_test_input.data.shape[0]):
     #     input = DTDP_test_input.data[index_of_image]
     #     pf.plot_results_FFN(input[:,0], prediction[:,:,0], test_input.labels[index_of_image,:,0], "Unet_fully_trained_on_"+test_sample_names[index_of_image])
--- src/training_functions.py
+++ src/training_functions.py
@@ -10,35 +10,12 @@ def scheduler(epoch, lr):
     return lr * exp(-0.1)
 
 
-
-def unet_OLD(train_input, test_input, length, weights = 'data/weights_NFI/weights_norm_avgpool.h5', train = False):
-    """unet training. Default is training and storing under weights.h5"""
-    number_of_dyes = 6
-    model = unet_small((length, number_of_dyes, 1))
-    model.summary()
-    model.load_weights(weights)
-
-    if train:
-        all_images = train_input.data
-        all_labels = train_input.labels
-        batch_size = 10  # number of samples processed before the model is updated
-        num_epochs = 10  # number of complete passes through the training dataset before the training stops
-        model_checkpoint = ModelCheckpoint(weights, monitor='val_loss', save_best_only=True)
-        # history is optional for plotting
-        history = model.fit(train_images, train_labels, batch_size=batch_size, epochs=num_epochs, verbose=1, shuffle=True, validation_split=0.2, callbacks=[model_checkpoint])
-        metric_values = model.evaluate(x=test_images, y=test_labels)
-        print('Final TEST performance')
-        for metric_value, metric_name in zip(metric_values, model.metrics_names):
-            print('{}: {}'.format(metric_name, metric_value))
-    return model
-
-
 def unet_train_test_split(train_input, test_input, length: int, weightpath = 'data/weights_NFI/weights_norm_avgpool.h5', train = False, batchsize = 10, epochs = 10):
     """unet training. Default is training and storing under weights.h5"""
     number_of_dyes = 6
     model = unet_small((length, number_of_dyes, 1))
     model.summary()
-    # model.load_weights(weightpath)
+    model.load_weights(weightpath)
     test_images = test_input.data
     test_labels = test_input.labels
 
@@ -49,8 +26,8 @@ def unet_train_test_split(train_input, test_input, length: int, weightpath = 'da
         num_epochs = epochs  # number of complete passes through the training dataset before the training stops
         model_checkpoint = ModelCheckpoint(weightpath, monitor='val_loss', save_best_only=True)
         # history is optional for plotting
-        lrscheduler = LearningRateScheduler(scheduler)
-        history = model.fit(train_images, train_labels, batch_size=batch_size, epochs=num_epochs, verbose=1, shuffle=True, validation_split=0.2, callbacks=[model_checkpoint, lrscheduler])
+        # lrscheduler = LearningRateScheduler(scheduler)
+        history = model.fit(train_images, train_labels, batch_size=batch_size, epochs=num_epochs, verbose=1, shuffle=True, validation_split=0.2, callbacks=[model_checkpoint])
     metric_values = model.evaluate(x=test_images, y=test_labels)
     print('Final TEST performance')
     for metric_value, metric_name in zip(metric_values, model.metrics_names):
