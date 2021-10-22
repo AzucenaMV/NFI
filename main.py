5ad97ff52094795d0b0aa9d6d821443f665c489c
Myrtyr <myrtevbelkom@gmail.com>
2021-10-22 15:08:04 +0200

    Trying to filter some results
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -21,56 +21,74 @@ PROVEDIt_raw_trace_data_SS = ['PROVEDIt_RD14-0003(100115ADG_15sec)_raw_improved1
 
 
 if __name__ == '__main__':
-    number_of_dyes = 6
-    leftoffset = 500
-    rightcutoff = 4800 + 500
-
-    train_samples = []
-    for elt in tracedata_for_training:
-        train_samples += rf.txt_read_sample(elt)
-    unnormalised_train, train_input, names_train = dpf.input_from_multiple_samples(train_samples, number_of_dyes, leftoffset, rightcutoff, True)
-
-    test_samples = []
-    for elt in tracedata_for_testing:
-        test_samples += rf.txt_read_sample(elt)
-    unnormalised_test, test_input, names_test = dpf.input_from_multiple_samples(test_samples, number_of_dyes, leftoffset, rightcutoff, True)
-    Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=False,epochs=100)
-
-    single_example = test_input.data[0].reshape(1,4800,6)
-    start_time_training = datetime.now()
-    Unet.predict(single_example)
-    end_time_training = datetime.now()
-
-    print('predicting single epg U-net: ', end_time_training-start_time_training)
+    # number_of_dyes = 6
+    # leftoffset = 500
+    # rightcutoff = 4800 + 500
+    #
+    # train_samples = []
+    # for elt in tracedata_for_training:
+    #     train_samples += rf.txt_read_sample(elt)
+    # unnormalised_train, train_input, names_train = dpf.input_from_multiple_samples(train_samples, number_of_dyes, leftoffset, rightcutoff, True)
+    #
+    # test_samples = []
+    # for elt in tracedata_for_testing:
+    #     test_samples += rf.txt_read_sample(elt)
+    # unnormalised_test, test_input, names_test = dpf.input_from_multiple_samples(test_samples, number_of_dyes, leftoffset, rightcutoff, True)
+    # Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=False,epochs=100)
 
 
-    width = 80
-    input_dim = 6*(width*2+1)
-    DTDP_train_input = dpf.DTDP_input_from_multiple_samples(train_samples, width = width)
-
-    DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
-    FFN_model, metrics = trf.FFN(DTDP_train_input, DTDP_test_input, weightpath="data/weights_DTDP/weights_for_timing.h5", inputsize=(input_dim,), train = False, epochs=100, batchsize=100)
-
-    just_the_data = DTDP_test_input.data
-    start_time_training = datetime.now()
-    for ind in range(4800):
-        FFN_model.predict(just_the_data[ind].reshape(1,966))
-    end_time_training = datetime.now()
-    print('predicting single epg FFN: ', end_time_training-start_time_training)
+    # width = 80
+    # input_dim = 6*(width*2+1)
+    # DTDP_train_input = dpf.DTDP_input_from_multiple_samples(train_samples, width = width)
+    #
+    # DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
+    # FFN_model, metrics = trf.FFN(DTDP_train_input, DTDP_test_input, weightpath="data/weights_DTDP/weights_for_timing.h5", inputsize=(input_dim,), train = False, epochs=100, batchsize=100)
+    #
+    # just_the_data = DTDP_test_input.data
+    # start_time_training = datetime.now()
+    # for ind in range(4800):
+    #     FFN_model.predict(just_the_data[ind].reshape(1,966))
+    # end_time_training = datetime.now()
+    # print('predicting single epg FFN: ', end_time_training-start_time_training)
 
     #     images, prediction = ppf.combine_results_FFN(DTDP_test_input, test_input, FFN_model, input_dim)
     # for index_of_image in range(DTDP_test_input.data.shape[0]):
     #     input = DTDP_test_input.data[index_of_image]
     #     pf.plot_results_FFN(input[:,0], prediction[:,:,0], test_input.labels[index_of_image,:,0], "Unet_fully_trained_on_"+test_sample_names[index_of_image])
-    # for sample_index in range(len(names)):
+    # for sample_index in range(len(names_test)):
     #     sample = test_input.data[sample_index]
     #     truth = test_input.labels[sample_index, :, 0]
     #     result = Unet.predict(sample.reshape(1,4800,6))[:,:,0].reshape(4800)
+    #     pf.plot_results_FFN(sample[:,0].reshape(4800), result, truth)
     #     auc = AUC()
     #     auc.update_state(truth, result)
     #     binacc = BinaryAccuracy()
     #     binacc.update_state(truth, result)
-    # dataframe = pd.read_csv('data/results_paper/FFNvUnet_bluedye.csv')
+    dataframe = pd.read_csv('data/results_paper/FFNvUnet_bluedye.csv')
+    avg_a = []
+    avg_b = []
+    avg_c = []
+    avg_d = []
+    avg_e = []
+    for ind, elt in dataframe.iterrows():
+        name = elt['name']
+        if name[1] == 'A':
+            avg_a.append(elt['auc_Unet'])
+        if name[1] == 'B':
+            avg_b.append(elt['auc_Unet'])
+        if name[1] == 'C':
+            avg_c.append(elt['auc_Unet'])
+        if name[1] == 'D':
+            avg_d.append(elt['auc_Unet'])
+        if name[1] == 'E':
+            avg_e.append(elt['auc_Unet'])
+        else:
+            print('oh no')
+    print(np.average(avg_a))
+    print(np.average(avg_b))
+    print(np.average(avg_c))
+    print(np.average(avg_d))
+    print(np.average(avg_e))
     # ax = dataframe.plot.scatter(x='auc_FFN', y = 'auc_Unet', color= 'darkviolet', marker = '*')
     # ax.plot(range(0,1,50), range(0,1,50))
     # ax.set_xlim([0.93,1])
