ccd13540574e5963b20945fcfea6eb5e1369e716
Myrtyr <myrtevbelkom@gmail.com>
2022-02-04 09:30:59 +0100

    Trained FFN for 400 epochs + saved weights
    Signed-off-by: Myrtyr <myrtevbelkom@gmail.com>

--- main.py
+++ main.py
@@ -23,33 +23,37 @@ tracedata_for_testing = ['TraceDataSet61.txt', 'TraceDataSet62.txt']
 
 if __name__ == '__main__':
 
-    my_model = unet_small()
-    my_model.summary()
-    new_model = MHCNN_DT()
-    new_model.summary()
+    # my_model = unet_small()
+    # my_model.summary()
+    # new_model = MHCNN_DT()
+    # new_model.summary()
 
-    # number_of_dyes = 6
-    # leftoffset = 500
-    # rightcutoff = 4800 + 500
-    #
-    # train_samples = []
-    # for elt in tracedata_for_training:
-    #     train_samples += rf.txt_read_sample(elt)
-    # unnormalised_train, train_input, names_train = dpf.input_from_multiple_samples(train_samples, number_of_dyes, leftoffset, rightcutoff, True)
-    #
-    # test_samples = []
-    # for elt in tracedata_for_testing:
-    #     test_samples += rf.txt_read_sample(elt)
-    # unnormalised_test, test_input, names_test = dpf.input_from_multiple_samples(test_samples, number_of_dyes, leftoffset, rightcutoff, True)
-    # Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=False,epochs=100)
+    number_of_dyes = 6
+    leftoffset = 500
+    rightcutoff = 4800 + 500
 
-    # width = 80
-    # input_dim = 6*(width*2+1)
-    # DTDP_train_input = dpf.DTDP_input_from_multiple_samples(train_samples, width = width)
-    #
-    # DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
-    # FFN_model, metrics = trf.FFN(DTDP_train_input, DTDP_test_input, weightpath="data/weights_DTDP/weights_for_timing.h5", inputsize=(input_dim,), train = False, epochs=100, batchsize=100)
-    #
+    train_samples = []
+    for elt in tracedata_for_training:
+        train_samples += rf.txt_read_sample(elt)
+
+    test_samples = []
+    for elt in tracedata_for_testing:
+        test_samples += rf.txt_read_sample(elt)
+
+    unnormalised_train, train_input, names_train = dpf.input_from_multiple_samples(train_samples, number_of_dyes, leftoffset, rightcutoff, True)
+    unnormalised_test, test_input, names_test = dpf.input_from_multiple_samples(test_samples, number_of_dyes, leftoffset, rightcutoff, True)
+    Unet = trf.unet_train_test_split(train_input, test_input, 4800, "data/weights_NFI/weights_clocktime.h5", train=False,epochs=100)
+
+    width = 80
+    input_dim = 6*(width*2+1)
+    DTDP_train_input = dpf.DTDP_input_from_multiple_samples(train_samples, width = width)
+
+    DTDP_test_input = dpf.DTDP_input_from_multiple_samples(test_samples, width = width)
+    start_time_training = datetime.now()
+    FFN_model, metrics = trf.FFN(DTDP_train_input, DTDP_test_input, weightpath="data_for_github/weights_FFN_400.h5", inputsize=(input_dim,), train = False, epochs=400, batchsize=100)
+    end_time_training = datetime.now()
+    print('Total training time (400 epochs): ', end_time_training-start_time_training)
+
     # just_the_data = DTDP_test_input.data
     # start_time_training = datetime.now()
     # for ind in range(4800):
@@ -57,10 +61,10 @@ if __name__ == '__main__':
     # end_time_training = datetime.now()
     # print('predicting single epg FFN: ', end_time_training-start_time_training)
 
-    #     images, prediction = ppf.combine_results_FFN(DTDP_test_input, test_input, FFN_model, input_dim)
+    # images, prediction = ppf.combine_results_FFN(DTDP_test_input, test_input, FFN_model, input_dim)
     # for index_of_image in range(DTDP_test_input.data.shape[0]):
     #     input = DTDP_test_input.data[index_of_image]
-    #     pf.plot_results_FFN(input[:,0], prediction[:,:,0], test_input.labels[index_of_image,:,0], "Unet_fully_trained_on_"+test_sample_names[index_of_image])
+    #     pf.plot_results_FFN(input[:,0], prediction[:,:,0], test_input.labels[index_of_image,:,0], "Unet_fully_trained_on_"+names_test[index_of_image])
     # for sample_index in range(len(names_test)):
     #     sample = test_input.data[sample_index]
     #     truth = test_input.labels[sample_index, :, 0]
